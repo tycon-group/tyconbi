abe37780cbf46a7bec232a3333a5f3923e963b17
tunogya <tunogya@qq.com>
2020-12-17 14:21:31 +0800

    完整引入Antd

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
 import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp(App);
+app.use(store).use(router).mount('#app');
+app.config.productionTip = false;
+app.use(Antd);
