6fb7473075d2e5e7d6f72355fd61d32bbef65089
leoforpeng <leo.peng@tyconcps.com>
2021-01-05 17:21:21 +0800

    目前的使用按需使用，需要调用

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
-import { Layout } from 'ant-design-vue';
+import { Layout, Menu } from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 const app = createApp(App);
 app.use(Layout);
+app.use(Menu);
 app.use(store).use(router).mount('#app');
 app.config.productionTip = false;
