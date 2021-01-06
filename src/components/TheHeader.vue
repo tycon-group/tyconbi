ad7f72d81e976910a19fc0ddf2b94e305d6e46c0
tunogya <tunogya@qq.com>
2021-01-06 20:03:40 +0800

    修正header布局，增加search按钮图标

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,19 +1,19 @@
 <template>
-  <a-layout-header class="header">
-    <div class="logo-box">
+  <a-layout-header class="box">
+    <div class="header">
       <img class="logo" src="../assets/logo_anti_white.png"
            alt="logo">
-    </div>
-    <div class="title">
-      <span>太江数据分析系统</span>
-    </div>
-    <div class="deadline">
-      <div class="text">
-        距离本期打分最终期限还有:
+      <span class="title">太江数据分析系统</span>
+      <div class="deadline">
+        <span class="text">本轮进度：</span>
+        <div style="width: 200px; height: 100px">
+          <a-progress :percent="50" size="small" status="active" />
+        </div>
       </div>
-      <div style="width: 200px; height: 100px">
-        <a-progress :percent="50" size="small" status="active" />
-      </div>
+    </div>
+    <div class="footer">
+      <img class="header-icon" src="../assets/search.png">
+      <img class="avatar" src="../assets/avatar.png">
     </div>
   </a-layout-header>
 </template>
@@ -31,38 +31,39 @@ export default {
 </script>
 
 <style scoped>
-.header {
+.box {
   background: #0080CC;
   position: fixed;
   padding: 0;
   display: flex;
   z-index: 1;
-  width: 100vw
+  width: 100vw;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.logo-box{
+.header{
   height: 64px;
-  width: 32px;
-  margin-left: 20px;
+  width: 760px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .title{
-}
-
-.title span{
+  height: 64px;
   font-family: SourceHanSansSC-Medium, serif;
   font-size: 20px;
   color: #FFFFFF;
   letter-spacing: 0;
   text-align: center;
-  margin-left: 20px;
 }
 
 .deadline {
   display: flex;
-  width: 500px;
+  width: 400px;
   height: 64px;
-  margin:0 auto;
+  margin-left: 20px;
   /*background-color: aquamarine;*/
 }
 
@@ -77,6 +78,28 @@ export default {
 .logo {
   width: 36px;
   height: 36px;
-  margin: 0 auto
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.footer{
+  height: 64px;
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.header-icon{
+  width: 26px;
+  height: 26px;
+  margin: 10px;
+}
+
+.avatar{
+  width: 30px;
+  height: 30px;
+  margin-right: 20px;
+  margin-left: 10px;
 }
 </style>
