e75250f95fb9892a6ed1389ea01187d4f2ec15ae
tunogya <tunogya@qq.com>
2021-01-07 18:23:01 +0800

    使用新的框架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,37 @@
+<!--<template>-->
+<!--&lt;!&ndash;    <the-header hidden="true"/>&ndash;&gt;-->
+<!--    <a-layout style="display: flex; padding-top: 64px; background: #13c2c2; height: 100vh">-->
+<!--      <a-layout style="width: 200px; background: #52c41a;height: 100vh " />-->
+<!--      <a-layout style="flex:auto;display: flex; flex-direction: column; background: #40a9ff">-->
+<!--        <a-layout style="flex: auto; background: #2c3e50"/>-->
+<!--        <a-layout style="height: 30px; background: #46a6ff"/>-->
+<!--      </a-layout>-->
+<!--    </a-layout>-->
+<!--</template>-->
+
 <template>
-  <a-layout>
-    <the-header hidden="true"/>
-    <a-layout style="display: flex; padding-top: 64px">
-      <the-sider />
-      <a-layout style="display: flex; flex-direction: column;flex: auto;">
-        <the-content style="flex: auto;"/>
-        <the-footer style="height: 30px"/>
-      </a-layout>
-    </a-layout>
-  </a-layout>
+  <!--    <the-header hidden="true"/>-->
+  <div style="display: flex; padding-top: 64px; background: #13c2c2; height: 100vh">
+    <div style="width: 200px; background: #52c41a;height: 100vh " />
+    <div style="flex:auto;display: flex; flex-direction: column; background: #40a9ff">
+      <div style="flex: auto; background: #2c3e50"/>
+      <div style="height: 30px; background: #46a6ff"/>
+    </div>
+  </div>
 </template>
+
 <script>
-import TheFooter from '../components/TheFooter.vue';
-import TheHeader from '../components/TheHeader.vue';
-import TheSider from '../components/TheSider.vue';
-import TheContent from '../components/TheContent.vue';
+// import TheFooter from '../components/TheFooter.vue';
+// import TheHeader from '../components/TheHeader.vue';
+// import TheSider from '../components/TheSider.vue';
+// import TheContent from '../components/TheContent.vue';
 
 export default {
   components: {
-    TheContent,
-    TheFooter,
-    TheHeader,
-    TheSider,
+    // TheContent,
+    // TheFooter,
+    // TheHeader,
+    // TheSider,
   },
 };
 </script>
