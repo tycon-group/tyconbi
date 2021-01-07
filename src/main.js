7b9c5567da0a2504b528486dfb6f31131aa45806
tunogya <tunogya@qq.com>
2021-01-07 16:08:35 +0800

    修改antd 为完整引入

--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
 import { createApp } from 'vue';
-import {
-  Layout, Menu, Slider, Avatar, Input, Progress, Button,
-} from 'ant-design-vue';
+import Antd from 'ant-design-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App);
-app.use(Layout);
-app.use(Menu);
-app.use(Slider);
-app.use(Avatar);
-app.use(Input);
-app.use(Progress);
-app.use(Button);
+app.use(Antd);
 app.use(store).use(router).mount('#app');
 app.config.productionTip = false;
