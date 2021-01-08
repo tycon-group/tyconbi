95a37afcaf1dd895caa424eb2563948b60ae8cb8
tunogya <tunogya@qq.com>
2021-01-08 10:16:25 +0800

    使用插槽改进layout，补充layout文件

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <a-layout style="height: 100vh">
+    <slot name="header">header</slot>
+    <a-layout style="padding-top: 64px;">
+      <slot name="sider">sider</slot>
+      <slot name="content">content</slot>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  name: 'base-layout',
+};
+</script>
+
+<style scoped>
+
+</style>
