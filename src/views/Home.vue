d758bf2270f086ec73bdf39712bf2bb91d089161
tunogya <tunogya@qq.com>
2021-01-08 10:16:04 +0800

    使用插槽改进layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,30 @@
 <template>
-  <a-layout style="height: 100vh">
-    <the-header />
-    <a-layout style="padding-top: 64px;">
-      <the-sider />
-      <the-content />
-    </a-layout>
-  </a-layout>
+    <base-layout>
+      <template v-slot:header>
+        <the-header/>
+      </template>
+      <template v-slot:sider>
+        <the-sider/>
+      </template>
+      <template v-slot:content>
+        <the-content/>
+      </template>
+    </base-layout>
 </template>
 
 <script>
 import TheHeader from '../components/TheHeader.vue';
 import TheSider from '../components/TheSider.vue';
 import TheContent from '../components/TheContent.vue';
+import BaseLayout from '../components/BaseLayout.vue';
 
 export default {
   components: {
     TheContent,
     TheHeader,
     TheSider,
+    BaseLayout,
+
   },
 };
 </script>
