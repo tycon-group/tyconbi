ac6287e561fd0b18dc8edf8653d225f709bb57af
tunogya <tunogya@qq.com>
2021-01-09 13:34:20 +0800

    确定KPI分析的界面初步框架

--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,13 +1,61 @@
 <template>
-  <p>日志分析111</p>
+  <div class="contanier">
+    <div class="navigation">
+<!--      组织架构-->
+    </div>
+    <div class="content">
+<!--      内容区域-->
+      <div class="header">
+<!--        分布图和成员信息-->
+      </div>
+      <div class="footer">
+<!--        Toolbar和列表-->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  // 这分析页面
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
 };
 </script>
 
 <style scoped>
-
+  .contanier {
+    display: flex;
+    justify-content: flex-start;
+    height: 100%;
+  }
+  .navigation {
+    /*background: white;*/
+    width: 274px;
+    height: 100%;
+    display: flex;
+    overflow: auto;
+    background: #a0d911;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    padding: 16px 16px 0;
+    flex: auto;
+    align-items: flex-start;
+    overflow: auto;
+  }
+  .header {
+    height: 272px;
+    background: #13c2c2;
+    width: 100%;
+  }
+  .footer {
+    flex: auto;
+    background: #40a9ff;
+    width: 100%;
+  }
 </style>
