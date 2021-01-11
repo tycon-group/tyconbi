3b0b4884034d8c83ac5e697dee46c24243676a2a
tunogya <tunogya@qq.com>
2021-01-11 16:28:32 +0800

    优化登陆界面

---
+++ src/components/BaseAppIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="width: 55px;
+                    height: 55px;
+                    position: relative;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin-right: 32px;">
+    <div style="position: absolute;" v-if="path">
+      <img style="width: 50px; height: 50px;" :src="path"/>
+    </div>
+    <img src="../assets/octo_gary.png"
+         style="width: 60px; height: 60px;" v-if="!path"/>
+    <img src="../assets/octo_white.png"
+         style="width: 60px; height: 60px;" v-else>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'base-app-icon',
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
