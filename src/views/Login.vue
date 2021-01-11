266d04b7f17be87553dc5eb9db094d9c55b7dc3c
leoforpeng <leo.peng@tyconcps.com>
2021-01-11 12:48:54 +0800

    登录完成

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,7 +9,7 @@
         </router-link>
       </div>
       <div style="height: 30px; width: 100%; font-size: 26px; text-align: start">
-        <span>Welcome Tycon BI</span>
+        <span>{{ title }}</span>
       </div>
       <div style="height: 210px;width: 100%">
         <a-form layout="vertical" :model="form" v-bind="formItemLayout" >
@@ -18,7 +18,7 @@
             <a-input
               required
               class="inputs"
-              v-model:value="form.fieldA"
+              v-model:value="form.username"
               maxlength="14"/>
           </a-form-item>
           <a-form-item style="font-size: 16px; text-align: start">
@@ -29,13 +29,15 @@
               class="inputs"
               type="password"
               autocomplete="off"
-              v-model:value="form.fieldB"
+              v-model:value="form.password"
               maxlength="18"/>
           </a-form-item>
           <a-form-item
-            style="font-size: 14px; text-align: start;"
-            :wrapper-col="buttonItemLayout.wrapperCol">
-            <a-button type="primary" style="width: 115px; height: 30px">
+            style="font-size: 14px; text-align: start;">
+            <a-button
+              type="primary"
+              html-type="submit"
+              style="width: 115px; height: 30px" @click="handleSubmit(user)">
               <span>登录</span>
             </a-button>
             <a
@@ -45,10 +47,26 @@
           </a-form-item>
         </a-form>
       </div>
-      <div style="background-color:red; height: 70px; margin-top: 70px;width: 100%">footer层</div>
+      <div style="display: flex; flex-direction: row; height: 50px; margin-top: 50px; width: 100%">
+        <div class="items">
+          <img class="imgs" src="../assets/logo_anti_white.png" alt="图标"/>
+        </div>
+        <div class="items" style="margin-left: 32px;">
+          <img class="imgs" src="#" alt="图标"/>
+        </div>
+        <div class="items" style="margin-left: 32px;">
+          <img class="imgs" src="#" alt="图标"/>
+        </div>
+        <div class="items" style="margin-left: 32px;">
+          <img class="imgs" src="#" alt="图标"/>
+        </div>
+      </div>
     </div>
     <div class="right">
-      <div class="bdPic">
+      <div class="bdPic"
+           style="width: 400px; height: 530px; background-color: #FFFFFF;">
+        <img src="../assets/bd_right.png"
+             style=" height: 100%; width: 100%;" alt="背景图"/>
       </div>
     </div>
   </div>
@@ -57,29 +75,29 @@
 export default {
   data() {
     return {
+      title: 'Welcome Tycon BI',
       form: {
-        fieldA: '',
-        fieldB: '',
+        username: '',
+        password: '',
       },
     };
   },
-  computed: {
-    formItemLayout() {
-      const { layout } = this.form;
-      return layout === 'horizontal'
-        ? {
-          labelCol: { span: 4 },
-          wrapperCol: { span: 14 },
-        }
-        : {};
-    },
-    buttonItemLayout() {
-      const { layout } = this.form;
-      return layout === 'horizontal'
-        ? {
-          wrapperCol: { span: 14, offset: 4 },
-        }
-        : {};
+  methods: {
+    handleSubmit(user) {
+      const param = new URLSearchParams();
+      param.append('username', this.forms.userName);
+      param.append('password', this.forms.password);
+      const instance = this.$http.create({ headers: { 'content-type': 'application/x-www-form-urlencoded' } });
+      instance.post('https://tyconcps.cn:8888/auth-token/', param).then((response) => {
+        console.log(response);
+        this.$message.success('登录成功');
+        const data1 = response.data;
+        console.log(data1, 'wewwwewe', user);
+        this.$router.push('/');
+      }).catch((error) => {
+        console.log(error);
+        this.$message.error('账号或密码错误');
+      });
     },
   },
 };
@@ -104,7 +122,10 @@ export default {
 }
 .right{
   width: 400px;
-  background: #52c41a;
+  background: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
 }
 
 .inputs {
@@ -117,4 +138,19 @@ export default {
   background: #2c3e50;
   width: 500px;
 }
+
+.items {
+  width: 50px;
+  height: 50px;
+  background-color: #cdcdcd;
+}
+.imgs {
+  width: 32px;
+  height: 32px;
+  margin: 0 auto;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
 </style>
