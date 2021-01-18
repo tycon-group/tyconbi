085331216b7b09134cc0b20ae51b02e8a9f82ea4
leoforpeng <leo.peng@tyconcps.com>
2021-01-18 18:22:30 +0800

    雷达图

--- src/components/BasePerson.vue
+++ src/components/BasePerson.vue
@@ -5,8 +5,11 @@
     <base-toolbar />
     <div>
       <a-tabs size="small">
-        <a-tab-pane key="1" tab="总览">
-          总揽
+        <a-tab-pane key="1" tab="总览" style="padding-left: 16px;">
+          <div style="margin-bottom: 20px; text-align: start;
+           font-size: 16px; font-weight: bold;">综合分析</div>
+          <div id="totalRadar"
+               style="padding-bottom: 16px; height: 175px; width: 100%"></div>
         </a-tab-pane>
         <a-tab-pane key="2" tab="工作能力">
           Content of Tab Pane 2
@@ -26,6 +29,8 @@
 </template>
 
 <script>
+import { Radar } from '@antv/g2plot';
+
 import BasePersonAttr from './BasePersonAttr.vue';
 import BaseToolbar from './BaseToolbar.vue';
 
@@ -34,6 +39,46 @@ export default {
   components: {
     BaseToolbar,
     BasePersonAttr,
+  },
+  mounted() {
+    const data = [
+      { name: 'G2', star: 10371 },
+      { name: 'G6', star: 7380 },
+      { name: 'F2', star: 7414 },
+      { name: 'L7', star: 2140 },
+      { name: 'X6', star: 660 },
+      { name: 'AVA', star: 885 },
+      { name: 'G2Plot', star: 1626 },
+    ];
+    const radarPlot = new Radar('totalRadar', {
+      data: data.map((d) => ({ ...d, star: Math.sqrt(d.star) })),
+      xField: 'name',
+      yField: 'star',
+      appendPadding: [0, 10, 0, 10],
+      meta: {
+        star: {
+          alias: 'star 数量',
+          min: 0,
+          nice: true,
+          formatter: (v) => Number(v).toFixed(2),
+        },
+      },
+      xAxis: {
+        tickLine: null,
+      },
+      yAxis: {
+        label: false,
+        grid: {
+          alternateColor: 'rgba(0, 0, 0, 0.04)',
+        },
+      },
+      // 开启辅助点
+      point: {
+        size: 2,
+      },
+      area: {},
+    });
+    radarPlot.render();
   },
 };
 </script>
---
+++ src/components/BaseTotalRadar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="total"></div>
+</template>
+
+<script>
+import { Radar } from '@antv/g2plot';
+
+export default {
+  name: 'base-total-radar',
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
