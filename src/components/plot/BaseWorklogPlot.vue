9b8d314ec30f1bb54d89f991a881eaee375b8d5b
leoforpeng <leo.peng@tyconcps.com>
2021-01-20 12:56:56 +0800

    添加多折线图2

---
+++ src/components/plot/BaseWorklogPlot.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="worklog" style="height: 174px; "></div>
+</template>
+
+<script>
+import { Line } from '@antv/g2plot';
+
+export default {
+  name: 'base-worklog-plot',
+  mounted() {
+    fetch('https://gw.alipayobjects.com/os/bmw-prod/e00d52f4-2fa6-47ee-a0d7-105dd95bde20.json')
+      .then((res) => res.json())
+      .then((data) => {
+        const linePlot = new Line('worklog', {
+          data,
+          xField: 'year',
+          yField: 'gdp',
+          seriesField: 'name',
+          yAxis: {
+            label: {
+              formatter: (v) => `${(v / 10e8).toFixed(1)} B`,
+            },
+          },
+          legend: {
+            position: 'top',
+          },
+          smooth: true,
+          // @TODO 后续会换一种动画方式
+          animation: {
+            appear: {
+              animation: 'path-in',
+              duration: 5000,
+            },
+          },
+        });
+        linePlot.render();
+      });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
