60c3fcc8cf551838b43b60897f389de1dd28aa4d
leoforpeng <leo.peng@tyconcps.com>
2021-01-20 11:08:53 +0800

    调整雷达图

--- src/components/BasePerson.vue
+++ src/components/BasePerson.vue
@@ -9,7 +9,7 @@
         <a-tab-pane key="1" tab="总览" style="padding-left: 16px;">
           <div style="margin-bottom: 20px; text-align: start;
            font-size: 16px; font-weight: bold;">综合分析</div>
-          <div id="totalRadar" style="height: 175px; width: 212px"></div>
+          <base-total-plot />
         </a-tab-pane>
         <a-tab-pane key="2" tab="工作能力">
           Content of Tab Pane 2
@@ -29,54 +29,17 @@
 </template>
 
 <script>
-import { Radar } from '@antv/g2plot';
 
 import BasePersonAttr from './BasePersonAttr.vue';
 import BaseToolbar from './BaseToolbar.vue';
+import BaseTotalPlot from './plot/BaseTotalPlot.vue';
 
 export default {
   name: 'base-person',
   components: {
+    BaseTotalPlot,
     BaseToolbar,
     BasePersonAttr,
-  },
-  mounted() {
-    const data = [
-      { name: '工作能力', star: 5371 },
-      { name: '工作态度', star: 7380 },
-      { name: '发展潜力', star: 7414 },
-      { name: '遵纪守法', star: 2140 },
-      { name: '突出贡献', star: 6160 },
-    ];
-    const radarPlot = new Radar('totalRadar', {
-      data: data.map((d) => ({ ...d, star: Math.sqrt(d.star) })),
-      xField: 'name',
-      yField: 'star',
-      appendPadding: [0, 25, 0, 25],
-      meta: {
-        star: {
-          alias: 'star 数量',
-          min: 0,
-          nice: true,
-          formatter: (v) => Number(v).toFixed(2),
-        },
-      },
-      xAxis: {
-        tickLine: null,
-      },
-      yAxis: {
-        label: false,
-        grid: {
-          alternateColor: 'rgba(0, 0, 0, 0.04)',
-        },
-      },
-      // 开启辅助点
-      point: {
-        size: 2,
-      },
-      area: {},
-    });
-    radarPlot.render();
   },
 };
 </script>
