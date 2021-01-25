acc5e8bbdb0609e83838f3ab8718792c531cbc30
leoforpeng <leo.peng@tyconcps.com>
2021-01-25 09:17:05 +0800

    添加测试后host

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,8 @@
 // 接口域名管理
 const base = {
   sso: 'https://tyconcps.cn:8888/',
-  bi: 'https://tyconcps.cn:4399/',
+  // bi: 'https://tyconcps.cn:4399/',
+  bi: 'http://10.10.1.5:8000/',
 };
 
 export default base;
