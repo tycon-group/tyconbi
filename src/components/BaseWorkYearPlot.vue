61d2d79a299d8d180d2395181a2e98c87b4958a5
leoforpeng <leo.peng@tyconcps.com>
2021-02-02 17:36:50 +0800

    工作年限图表

--- src/components/BaseWorkYearPlot.vue
+++ src/components/BaseWorkYearPlot.vue
@@ -7,61 +7,52 @@ import { Column } from '@antv/g2plot';
 
 export default {
   name: 'base-work-year-plot',
-  mounted() {
-    const data = [
-      {
-        type: '一年以内',
-        sales: 8,
-      },
-      {
-        type: '1-5年',
-        sales: 21,
-      },
-      {
-        type: '5-10年',
-        sales: 41,
-      },
-      {
-        type: '10-15年',
-        sales: 5,
-      },
-      {
-        type: '15年以上',
-        sales: 5,
-      },
-    ];
-    const columnPlot = new Column('workYearColumn', {
-      data,
-      xField: 'type',
-      yField: 'sales',
-      label: {
-        // 可手动配置 label 数据标签位置
-        position: 'middle', // 'top', 'bottom', 'middle',
-        // 配置样式
-        style: {
-          fill: '#FFFFFF',
-          opacity: 0.6,
-        },
-      },
-      xAxis: {
+  props: ['deptWorkingYear'],
+  watch: {
+    orgWorkingYear() {
+      const data = this.deptWorkingYear;
+      const columnPlot = new Column('workYearColumn', {
+        data,
+        xField: 'name',
+        yField: 'value',
         label: {
-          autoHide: true,
-          autoRotate: false,
+          // 可手动配置 label 数据标签位置
+          position: 'middle', // 'top', 'bottom', 'middle',
+          // 配置样式
+          style: {
+            fill: '#FFFFFF',
+            opacity: 0.6,
+          },
         },
-      },
-      meta: {
-        type: {
-          alias: '类别',
+        xAxis: {
+          label: {
+            autoHide: true,
+            autoRotate: false,
+          },
         },
-        sales: {
-          alias: '数量',
+        meta: {
+          name: {
+            alias: '类别',
+          },
+          value: {
+            alias: '数量',
+          },
         },
-      },
-      minColumnWidth: 50,
-      maxColumnWidth: 50,
-    });
-    columnPlot.render();
+        minColumnWidth: 50,
+        maxColumnWidth: 50,
+      });
+      columnPlot.render();
+      setInterval(() => {
+        columnPlot.changeData(this.deptWorkingYear);
+      }, 200);
+    },
   },
+  computed: {
+    orgWorkingYear() {
+      return this.deptWorkingYear;
+    },
+  },
+
 };
 </script>
 
