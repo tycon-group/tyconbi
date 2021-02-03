036506bb842c041239fc1fa94a94d7e6a6bc7bd0
tunogya <tunogya@qq.com>
2021-02-03 13:38:49 +0800

    更新sso

--- src/api/base.js
+++ src/api/base.js
@@ -1,6 +1,6 @@
 // 接口域名管理
 const base = {
-  sso: 'https://tyconcps.cn:8888/v1/',
+  sso: 'https://sso.tyconcps.com/v1/',
   // bi: 'https://tyconcps.cn:4399/',
   bi: 'http://127.0.0.1:8000/v2/',
 };
