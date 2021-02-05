6f9a2aab61dc2000d122a44521b1c57f04adc044
leoforpeng <leo.peng@tyconcps.com>
2021-02-05 11:32:22 +0800

    组织取消，数据优化展示

--- src/components/BasePersonAttr.vue
+++ src/components/BasePersonAttr.vue
@@ -140,19 +140,19 @@ export default {
         this.gender = '未知';
       }
       // 生日
-      if (this.personItems.dateOfBirth !== null) {
+      if (this.personItems.dateOfBirth !== null && this.personItems.dateOfBirth !== undefined) {
         this.dateOfBirth = this.personItems.dateOfBirth;
       } else {
         this.dateOfBirth = '未知信息';
       }
       // 学历
-      if (this.personItems.education !== null) {
+      if (this.personItems.education !== null && this.personItems.education !== undefined) {
         this.education = this.personItems.education;
       } else {
         this.education = '未知信息';
       }
       // 职位
-      if (this.personItems.job !== null) {
+      if (this.personItems.job !== null && this.personItems.job !== undefined) {
         this.job = this.personItems.job;
       } else {
         this.job = '未知信息';
@@ -162,7 +162,7 @@ export default {
       // 连续工作时间
       // 获取开始工作日期
       const { dateOfStartWork } = this.personItems;
-      if (dateOfStartWork !== null) {
+      if (dateOfStartWork !== null && dateOfStartWork !== undefined) {
         const date2 = new Date(dateOfStartWork);
         // eslint-disable-next-line radix,no-mixed-operators,max-len
         const monthCount = parseInt(date1.getFullYear() - date2.getFullYear()) * 12 - date2.getMonth() + date1.getMonth();
@@ -179,7 +179,7 @@ export default {
       }
       // 在职工作年限
       const { dateOfHire } = this.personItems;
-      if (dateOfHire !== null) {
+      if (dateOfHire !== null && dateOfHire !== undefined) {
         const date2 = new Date(dateOfHire);
         // eslint-disable-next-line radix,no-mixed-operators,max-len
         const monthCount = parseInt(date1.getFullYear() - date2.getFullYear()) * 12 - date2.getMonth() + date1.getMonth();
