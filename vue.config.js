7d0462de69146b95d4b301348a7983cc1c7e16af
tunogya <tunogya@qq.com>
2021-02-08 15:55:54 +0800

    删除token

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,6 @@ module.exports = {
           provider: 'github',
           repo: 'https://github.com/tycon-group/tyconbi',
           owner: 'tycon-group',
-          token: '9e35bcfa3260fc7d31c891bfe1b1a3827e5b9965',
           releaseType: 'release',
           publishAutoUpdate: true,
         },
